58ba59b2ebfa087d324a8ffc78dc656124f54919
mostafa <mtgwely0100@gmail.com>
2020-03-23 00:29:21 +0200

    initial commit for memento pattern

--- src/com/mostafapackage/Main.java
+++ src/com/mostafapackage/Main.java
@@ -1,8 +1,24 @@
 package com.mostafapackage;
 
+import com.mostafapackage.memento.Editor;
+import com.mostafapackage.memento.History;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+
     }
 }
